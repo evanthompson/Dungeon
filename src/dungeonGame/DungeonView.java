0e91531e31610ab3fee7a57e0949acb2167ada6a
evanthompson <ethompson.cs@gmail.com>
2013-06-03 02:06:52 -0400

    Imported Custom Font
    Baumans font installed onto window and used in game..
    Probably won't be viewable on other computers

--- src/dungeonGame/DungeonView.java
+++ src/dungeonGame/DungeonView.java
@@ -262,7 +262,7 @@ public class DungeonView implements Observer {
 		addResource(dGray = new Color(shell.getDisplay(), 30, 30, 30));
 		addResource(gray = new Color(shell.getDisplay(), 150, 150, 150));
 		addResource(lGray = new Color(shell.getDisplay(), 200, 200, 200));
-		addResource(titleFont = new Font(shell.getDisplay(), "Helvetica", 24, SWT.BOLD));
+		addResource(titleFont = new Font(shell.getDisplay(), "Baumans", 24, SWT.BOLD));
 	}
 	
 	public void createImages() {
